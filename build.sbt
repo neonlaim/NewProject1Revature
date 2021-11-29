8ccfb462b602be1bf8bc6f5cd35a5d1180c878a0
DK <neonlaim@gmail.com>
2021-11-29 07:52:39 -0800

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "NewProject1Revature"
+
+version := "0.1"
+
+scalaVersion := "2.13.7"
---
+++ src/main/scala/Start.scala
@@ -0,0 +1,5 @@
+object Start {
+  def main(args: Array[String]) = {
+    println("Hello, world")
+  }
+}
